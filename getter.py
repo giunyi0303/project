557d91603c2d8fe8700399e00426a8c716b72f3e
giunyi0303 <giunyi0303@gmail.com>
2024-06-12 15:18:43 +0900

    addexception

--- getter.py
+++ getter.py
@@ -16,32 +16,26 @@ import os
 
 load_dotenv()
 project_info = {
-    'ADH_project': {'username':os.getenv('ADH_id'), 'password':os.getenv('ADH_pw')},
-    'SPR_project': {'username':os.getenv('SPR_id'), 'password':os.getenv('SPR_pw')},
-    'WKH_project': {'username':os.getenv('WKH_id'), 'password':os.getenv('WKH_pw')},
+    'ADH_project': {'username': os.getenv('ADH_id'), 'password': os.getenv('ADH_pw')},
+    'SPR_project': {'username': os.getenv('SPR_id'), 'password': os.getenv('SPR_pw')},
+    'WKH_project': {'username': os.getenv('WKH_id'), 'password': os.getenv('WKH_pw')},
 }
 
 def dynamic_web_crawler(url, info):
-    # 크롬 옵션 창 열지 않기 
     options = Options()
     options.add_argument('--headless') 
 
-    # instance 생성 
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
     try:
         driver.get(url)
-
-        # 열릴때까지 기다리기 
         wait = WebDriverWait(driver, 10)
         username_field = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="id"]')))
         password_field = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="password"]')))
         
-        # 로그인 값 입력하기 
         username_field.send_keys(info['username'])
         password_field.send_keys(info['password'])
 
-        # 로그인 버튼 찾기 
         login_button = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="__next"]/div[1]/div/div[3]/div[1]/form/button')))
         login_button.click()
         
@@ -54,30 +48,23 @@ def dynamic_web_crawler(url, info):
         
         
 def dynamic_web_crawler_SPR(url, info):
-    # 크롬 옵션 창 열지 않기 
     options = Options()
-    options.add_argument('--headless') 
+    # options.add_argument('--headless') 
 
-    # instance 생성 
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
     try:
         driver.get(url)
-
-        # 열릴때까지 기다리기 
         wait = WebDriverWait(driver, 20)
         username_field = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="id"]')))
         password_field = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="password"]')))
         
-        # 로그인 값 입력하기 
         username_field.send_keys(info['username'])
         password_field.send_keys(info['password'])
 
-        # 로그인 버튼 찾기 
         login_button = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="__next"]/div[1]/div/div[3]/div[1]/form/button')))
         login_button.click()
         
-        # 드롭다운 버튼을 클릭하여 리스트 열기
         list_button = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="site-select"]')))
         
         def click_element(element):
@@ -88,10 +75,8 @@ def dynamic_web_crawler_SPR(url, info):
         
         click_element(list_button)
         
-        # 드롭다운 메뉴가 열리기를 기다리기
         list_menu = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="menu-"]/div[3]')))
-        list = list_menu.text
-        agency_list = list.split('\n')
+        agency_list = list_menu.text.split('\n')
 
         result = []
 
@@ -100,19 +85,17 @@ def dynamic_web_crawler_SPR(url, info):
                 agency_element = wait.until(EC.presence_of_element_located((By.XPATH, f'//li[text()="{agency}"]')))
                 click_element(agency_element)
                 
+                short_wait = WebDriverWait(driver, 5)
                 try:
-                    info_element = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="__next"]/main/div/div/div[2]/div[2]/div[1]/div[1]/div/p')))
+                    info_element = short_wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="__next"]/main/div/div/div[2]/div[2]/div[1]/div[1]/div/p')))
                     info_text = info_element.text
                     value = info_text
                 except TimeoutException:
                     info_text = "할당없음"
                     value = info_text
-                    # 뒤로가기
                     driver.back()
-                    # 페이지 새로고침
                     driver.refresh()
-                    # 드롭다운 버튼을 클릭하여 리스트 열기
-                    list_button = wait.until(EC.presence_of_element_located((By.XPATH,'//*[@id="site-select"]')))
+                    list_button = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="site-select"]')))
                     click_element(list_button)
 
                 print(f"Agency: {agency}")
@@ -120,9 +103,8 @@ def dynamic_web_crawler_SPR(url, info):
 
                 result.append((agency, value))
                 
-                # 드롭다운 메뉴 다시 열기 (마지막 항목이 아닌 경우)
                 if i < len(agency_list) - 1:
-                    list_button = wait.until(EC.presence_of_element_located((By.XPATH,'//*[@id="site-select"]')))
+                    list_button = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="site-select"]')))
                     click_element(list_button)
             except TimeoutException:
                 print(f"Agency element not found for: {agency}")
@@ -134,7 +116,6 @@ def dynamic_web_crawler_SPR(url, info):
         driver.quit()
 
 
-# google API 접속 
 scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
 json_keyfile_name = os.getenv('json_keyfil_name')
 credentials = ServiceAccountCredentials.from_json_keyfile_name(json_keyfile_name, scope)
@@ -146,24 +127,52 @@ url = os.getenv('url')
 for project, info in project_info.items():
     print(f"Logging in for project: {project}")
     if project == 'SPR_project':
-        results = dynamic_web_crawler_SPR(url , info)
-        for agency, value in results:
-            print(f"Agency: {agency}, Value: {value}")
-            # 현재 시간 가져오기
-            current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            
-            # 현재 시트에 데이터 추가
-            worksheet = doc.worksheet(project)
-            row_data = [project, current_time, agency, value]
-            worksheet.append_row(row_data)
+        try:
+            results = dynamic_web_crawler_SPR(url , info)
+            if results is not None:
+                for agency, value in results:
+                    # 값이 '할당없음'이 아니고 퍼센트 형식인 경우에만 변환
+                    if value != '할당없음' and value.endswith('%'):
+                        value = f"{float(value.strip('%')):.1f}%"
+                    print(f"Agency: {agency}, Value: {value}")
+                    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                    worksheet = doc.worksheet(project)
+                    row_data = [project, current_time, agency, value]
+                    worksheet.append_row(row_data)
+        except Exception as e:
+            print(f"An error occurred: {str(e)}")
+            print("Retrying...")
+            results = dynamic_web_crawler_SPR(url , info)
+            if results is not None:
+                for agency, value in results:
+                    if value != '할당없음' and value.endswith('%'):
+                        value = f"{float(value.strip('%')):.1f}%"
+                    print(f"Agency: {agency}, Value: {value}")
+                    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                    worksheet = doc.worksheet(project)
+                    row_data = [project, current_time, agency, value]
+                    worksheet.append_row(row_data)
+            else:
+                print("No results obtained.")
     else:  
-        value = dynamic_web_crawler(url, info)
-        print(f"Value extracted: {value}")
-        # 현재 시간 가져오기
-        current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        
-        # 현재 시트에 데이터 추가
-        worksheet = doc.worksheet(project)
-        row_data = [project, current_time, value]
-        worksheet.append_row(row_data)
-
+        try:
+            value = dynamic_web_crawler(url, info)
+            # 값이 '할당없음'이 아니고 퍼센트 형식인 경우에만 변환
+            if value != '할당없음' and value.endswith('%'):
+                value = f"{float(value.strip('%')):.1f}%"
+            print(f"Value extracted: {value}")
+            current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            worksheet = doc.worksheet(project)
+            row_data = [project, current_time, value]
+            worksheet.append_row(row_data)
+        except Exception as e:
+            print(f"An error occurred: {str(e)}")
+            print("Retrying...")
+            value = dynamic_web_crawler(url, info)
+            if value != '할당없음' and value.endswith('%'):
+                value = f"{float(value.strip('%')):.1f}%"
+            print(f"Value extracted: {value}")
+            current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            worksheet = doc.worksheet(project)
+            row_data = [project, current_time, value]
+            worksheet.append_row(row_data)
